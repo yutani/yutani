4dc69d464ad13da33acc9432565af46d68db041c
Luke Bennellick <bennellick.luke@gmail.com>
2014-10-25 23:07:11 +0100

    Add basic configuration to application

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,4 @@
-
+# This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
@@ -7,9 +7,13 @@ require 'capybara/rspec'
 
 ActiveRecord::Migration.maintain_test_schema!
 
+Capybara.configure do |config|
+  config.match = :prefer_exact
+end
+
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
   config.infer_spec_type_from_file_location!
 end
