e3fa22943bd54d755601ded331ec0da29c61fa6e
Luke Bennellick <bennellick.luke@gmail.com>
2014-10-26 08:46:15 +0000

    Install simple form, add has_secure_password and index view to User model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,30 @@
 class UsersController < ApplicationController
+  def index
+    @user = User.all
+  end
+
   def new
     @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path, :notice => "Successfully created user"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :password,
+      :password_confirmation
+    )
   end
 end
