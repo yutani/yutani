6d04b371983db70f48d03b629ddea546e1214bd3
Luke Bennellick <bennellick.luke@gmail.com>
2014-10-26 17:34:25 +0000

    Merge tests

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -12,16 +12,6 @@ RSpec.describe "Signing up" do
       fill_in "Password confirmation", :with => "Secret123"
 
       expect { click_on "Create Account" }.to change(User, :count).by(1)
-    end
-
-    it "notifies user of success" do
-      fill_in "First name",    :with => "Luke Bennellick"
-      fill_in "Last name",     :with => "Bennellick"
-      fill_in "Email",         :with => "example@test.com"
-      fill_in "Password",      :with => "Secret123"
-      fill_in "Password confirmation", :with => "Secret123"
-      click_on("Create Account")
-
       expect(page).to have_content(/account created/i)
     end
   end
